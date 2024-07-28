0908e2e7df6c4d75a1dc143ed390d7dc9531e38b
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-07-28 23:40:34 +0300

    reset css

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./Components/MainPage/MainPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="" element={<MainPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
