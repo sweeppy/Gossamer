8e222a46f398a69a286cc09992b226bd4451c336
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-08-03 23:25:59 +0300

    Update features block(css)

--- Frontend/src/Components/MainPage/Features.tsx
+++ Frontend/src/Components/MainPage/Features.tsx
@@ -3,49 +3,47 @@ const Features = () => {
     <section className="padding-block-900">
       <div className="container">
         <div className="features-container">
-          <div className="items">
-            <div className="item-box bg-gray">
-              <h3 className=" fs-600 fw-bold">
-                Personalized Book Recommendations:
-              </h3>
-              <article className="fs-300 fw-regular">
-                Receive tailored book suggestions based on your reading history
-                and preferences. Gossamer's intelligent algorithm analyzes your
-                favorite genres, authors, and past reviews to recommend books
-                that are just right for you.
-              </article>
-            </div>
-            <div className="item-box bg-gray">
-              <h3 className=" fs-600 fw-bold">
-                Community Reviews and Ratings:
-              </h3>
-              <article className="fs-300 fw-regular">
-                Engage with a vibrant community of readers by sharing your
-                thoughts on books you've read. Rate and review books to help
-                others discover their next great read and explore diverse
-                opinions and discussions.
-              </article>
-            </div>
-            <div className="item-box bg-gray">
-              <h3 className=" fs-600 fw-bold">
-                Custom Reading Lists and Shelves:
-              </h3>
-              <article className="fs-300 fw-regular">
-                Organize your literary journey with custom reading lists and
-                shelves. Whether it's a list of books to read next, favorite
-                novels, or a collection of completed reads, Gossamer lets you
-                manage your reading life with ease.
-              </article>
-            </div>
-            <div className="item-box bg-gray">
-              <h3 className=" fs-600 fw-bold">Author and Genre Insights:</h3>
-              <article className="fs-300 fw-regular">
-                Dive deeper into the world of books with detailed insights into
-                authors and genres. Explore author biographies, similar books,
-                and genre trends to broaden your literary horizons and discover
-                new favorites.
-              </article>
-            </div>
+          <div className="item-box bg-gray">
+            <h3 className="fs-small fw-bold">
+              Personalized Book Recommendations:
+            </h3>
+            <article className="fs-xs fw-regular">
+              Receive tailored book suggestions based on your reading history
+              and preferences. Gossamer's intelligent algorithm analyzes your
+              favorite genres, authors, and past reviews to recommend books that
+              are just right for you.
+            </article>
+          </div>
+          <div className="item-box bg-gray">
+            <h3 className=" fs-small fw-bold">
+              Community Reviews and Ratings:
+            </h3>
+            <article className="fs-xs fw-regular">
+              Engage with a vibrant community of readers by sharing your
+              thoughts on books you've read. Rate and review books to help
+              others discover their next great read and explore diverse opinions
+              and discussions.
+            </article>
+          </div>
+          <div className="item-box bg-gray">
+            <h3 className=" fs-small fw-bold">
+              Custom Reading Lists and Shelves:
+            </h3>
+            <article className="fs-xs fw-regular">
+              Organize your literary journey with custom reading lists and
+              shelves. Whether it's a list of books to read next, favorite
+              novels, or a collection of completed reads, Gossamer lets you
+              manage your reading life with ease.
+            </article>
+          </div>
+          <div className="item-box bg-gray">
+            <h3 className=" fs-small fw-bold">Author and Genre Insights:</h3>
+            <article className="fs-xs fw-regular">
+              Dive deeper into the world of books with detailed insights into
+              authors and genres. Explore author biographies, similar books, and
+              genre trends to broaden your literary horizons and discover new
+              favorites.
+            </article>
           </div>
         </div>
       </div>
