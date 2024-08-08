e4d7eadd585dbd2c7b22c17228cb122224c91d5a
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-08-08 17:39:23 +0300

    Add alternative authentication

--- Frontend/src/Components/Auth/Login/Login.tsx
+++ Frontend/src/Components/Auth/Login/Login.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-
 const Login = () => {
   return (
     <main>
       <div className="container auth-container">
-        <header className="padding-block-900">
-          <h1 className="fs-xl ding fw-bold">Log in</h1>
-        </header>
+        <h1 className="fs-xl ding fw-bold FadeInTopSlide padding-block-900">
+          Log in
+        </h1>
+        <div className="group">
+          <button data-google className="button long-button">
+            Continue with Google
+            <img src="" alt="" />
+          </button>
+          <button data-apple className="button long-button">
+            Continue with Apple
+            <img src="" alt="" />
+          </button>
+        </div>
       </div>
     </main>
   );
