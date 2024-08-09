e7ef328ce16b74cead0355495181323207f6d40b
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-08-09 18:37:11 +0300

    Add input field for login page

--- Frontend/src/Components/Auth/Login/Login.tsx
+++ Frontend/src/Components/Auth/Login/Login.tsx
@@ -2,12 +2,22 @@ import AlternativeLogin from "./AlternativeLogin";
 
 const Login = () => {
   return (
-    <main>
+    <main className="rubik">
       <div className="container auth-container">
-        <h1 className="fs-xl ding fw-bold FadeInTopSlide padding-block-900">
+        <h1 className="fs-xl fw-bold FadeInTopSlide padding-block-900">
           Log in
         </h1>
         <AlternativeLogin />
+        <div className="padding-block-900" style={{ width: "100%" }}>
+          <label className="sub-header">Email</label>
+          <div className="input-container">
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter your email address..."
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
