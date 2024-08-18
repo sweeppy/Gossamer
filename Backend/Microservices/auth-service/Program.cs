37bf9fcd2f553ebc8c4e2c3e83172bfd481deeab
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-08-18 14:00:29 +0300

    Add Dockerfile to auth-service

--- Backend/Microservices/auth-service/Program.cs
+++ Backend/Microservices/auth-service/Program.cs
@@ -1,7 +1,11 @@
+using auth_service.src.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<UserDbContext>();
 
 var app = builder.Build();
 
